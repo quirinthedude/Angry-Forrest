da8475848ef98bf55be016f5bed8276afc1be2dc
Quirin Pflaum <quirin.pflaum@proton.me>
2026-08-05 16:18:54 +0200

    first object rendered in canvas

--- js/models/World.class.js
+++ js/models/World.class.js
@@ -1,12 +1,20 @@
 class World {
   character = new Character();
-  enemies = [new Golem(100, 100), new Robot(300, 300)];
+  enemies = [
+    new Golem(100, 100),
+    new Golem(120, 100),
+    new Golem(140, 100),
+    new Golem(160, 100),
+    new Robot(300, 300),
+  ];
   ctx;
+  canvas;
 
   constructor(canvas) {
     this.canvas = canvas;
     this.ctx = canvas.getContext("2d");
     this.character.img.onload = () => this.draw();
+    //   this.enemies.forEach((enemy) => this.enemy.img.draw); noch nicht ganz!
     this.draw();
   }
 
@@ -33,13 +41,7 @@ class World {
     this.ctx.save();
     this.ctx.translate(object.x + object.width, object.y);
     this.ctx.scale(-1, 1);
-    this.ctx.drawImage(
-      object.img,
-      0,
-      0,
-      object.width,
-      object.height,
-    );
+    this.ctx.drawImage(object.img, 0, 0, object.width, object.height);
     this.ctx.restore();
   }
 }
