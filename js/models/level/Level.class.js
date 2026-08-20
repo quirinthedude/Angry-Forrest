9668d84da0a1fdc39d28ae06ae751f94c0779fe6
Quirin Pflaum <quirin.pflaum@proton.me>
2026-08-20 09:13:35 +0200

    setting up Level

---
+++ js/models/level/Level.class.js
@@ -0,0 +1,11 @@
+class Level {
+  enemies;
+  sky;
+  landscape;
+
+  constructor(enemies, sky, landscape) {
+    this.enemies = enemies;
+    this.sky = sky;
+    this.landscape = landscape;
+  }
+}
