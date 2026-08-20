6a9be4fe9135f84eb8d58be44ebd2a431787388f
Quirin Pflaum <quirin.pflaum@proton.me>
2026-08-20 12:52:34 +0200

    adding animations-frames for robot and golems

--- js/models/Golem.class.js
+++ js/models/Golem.class.js
@@ -1,15 +1,89 @@
 class Golem extends MovableObject {
   height = 120;
   width = 120;
-  IMAGES_WALKING = createAnimationImages(
-    "/img/golem/Walking/Golem_Walking_",
-    22,
+  IMAGES_IDLE = createAnimationImages(
+    "./img/golem/Idle/Golem_Idle_",
+    18,
   );
+  IMAGES_IDLE_BLINKING = createAnimationImages(
+    "./img/golem/Idle Blinking/Golem_Idle Blinking_",
+    18,
+  );
+  IMAGES_WALKING = createAnimationImages(
+    "./img/golem/Walking/Golem_Walking_",
+    24,
+  );
+  IMAGES_RUNNING = createAnimationImages(
+    "./img/golem/Running/Golem_Running_",
+    12,
+  );
+  IMAGES_ATTACKING = createAnimationImages(
+    "./img/golem/Slashing/Golem_Slashing_",
+    12,
+  );
+  IMAGES_ATTACKING_AIR = createAnimationImages(
+    "./img/golem/Slashing in The Air/Golem_Slashing in The Air_",
+    12,
+  );
+  IMAGES_THROWING = createAnimationImages(
+    "./img/golem/Throwing/Golem_Throwing_",
+    12,
+  );
+  IMAGES_THROWING_AIR = createAnimationImages(
+    "./img/golem/Throwing in The Air/Golem_Throwing in The Air_",
+    12,
+  );
+  IMAGES_HURT = createAnimationImages("./img/golem/Hurt/Golem_Hurt_", 12);
+  IMAGES_JUMPING = createAnimationImages(
+    "./img/golem/Jump Loop/Golem_Jump Loop_",
+    6,
+  );
+  IMAGES_JUMP_START = createAnimationImages(
+    "./img/golem/Jump Start/Golem_Jump Start_",
+    6,
+  );
+  IMAGES_KICKING = createAnimationImages(
+    "./img/golem/Kicking/Golem_Kicking_",
+    12,
+  );
+  IMAGES_RUN_SLASHING = createAnimationImages(
+    "./img/golem/Run Slashing/Golem_Run Slashing_",
+    12,
+  );
+  IMAGES_RUN_THROWING = createAnimationImages(
+    "./img/golem/Run Throwing/Golem_Run Throwing_",
+    12,
+  );
+  IMAGES_SLIDING = createAnimationImages(
+    "./img/golem/Sliding/Golem_Sliding_",
+    6,
+  );
+  IMAGES_FALLING_DOWN = createAnimationImages(
+    "./img/golem/Falling Down/Golem_Falling Down_",
+    6,
+  );
+  IMAGES_DYING = createAnimationImages("./img/golem/Dying/Golem_Dying_", 15);
 
   constructor(x, y) {
     super();
-    this.loadImage(this.IMAGES_WALKING[0]);
-    this.loadImages(this.IMAGES_WALKING, 22, 50);
+    this.loadImage(this.IMAGES_IDLE[0]);
+    this.loadImages(this.IMAGES_IDLE);
+    this.loadImages(this.IMAGES_IDLE_BLINKING);
+    this.loadImages(this.IMAGES_WALKING);
+    this.loadImages(this.IMAGES_RUNNING);
+    this.loadImages(this.IMAGES_ATTACKING);
+    this.loadImages(this.IMAGES_ATTACKING_AIR);
+    this.loadImages(this.IMAGES_THROWING);
+    this.loadImages(this.IMAGES_THROWING_AIR);
+    this.loadImages(this.IMAGES_HURT);
+    this.loadImages(this.IMAGES_JUMPING);
+    this.loadImages(this.IMAGES_JUMP_START);
+    this.loadImages(this.IMAGES_KICKING);
+    this.loadImages(this.IMAGES_RUN_SLASHING);
+    this.loadImages(this.IMAGES_RUN_THROWING);
+    this.loadImages(this.IMAGES_SLIDING);
+    this.loadImages(this.IMAGES_FALLING_DOWN);
+    this.loadImages(this.IMAGES_DYING);
     this.x = x;
     this.y = y;
     this.mirrorX = true;
--- js/models/Robot.class.js
+++ js/models/Robot.class.js
@@ -1,9 +1,52 @@
 class Robot extends MovableObject {
   height = 160;
   width = 160;
+  IMAGES_IDLE = createAnimationImages("./img/robot-boss/Idle/idle_", 9);
+  IMAGES_WALKING = createAnimationImages("./img/robot-boss/Walk/Walk_", 12);
+  IMAGES_RUNNING = createAnimationImages("./img/robot-boss/Run/Run_", 12);
+  IMAGES_RUN_ATTACKING = createAnimationImages(
+    "./img/robot-boss/Run_Attack/Run_Attack_",
+    12,
+  );
+  IMAGES_RUN_SHOOTING = createAnimationImages(
+    "./img/robot-boss/Run_Shot/Run_Shot_",
+    12,
+  );
+  IMAGES_ATTACKING = createAnimationImages(
+    "./img/robot-boss/Attack/Attack_",
+    18,
+  );
+  IMAGES_SHOOTING = createAnimationImages("./img/robot-boss/Shot/Shot_", 16);
+  IMAGES_JUMPING = createAnimationImages("./img/robot-boss/Jump/Jump_", 13);
+  IMAGES_SITTING = createAnimationImages("./img/robot-boss/Sit/Sit_", 15);
+  IMAGES_BOMBING = createAnimationImages("./img/robot-boss/Bomb/Bomb_", 20);
+  IMAGES_TURNING_TO_RUN = createAnimationImages(
+    "./img/robot-boss/Turn_to_Run/Turn_to_run_",
+    4,
+  );
+  IMAGES_TURNING_TO_WALK = createAnimationImages(
+    "./img/robot-boss/Turn_to_walk/Turn_to_walk_",
+    4,
+  );
+  IMAGES_DYING = createAnimationImages("./img/robot-boss/Death/Death_", 15);
 
   constructor(x, y) {
-    super().loadImage("./img/robot-boss/Idle/idle_000.png");
+    super();
+    this.loadImage(this.IMAGES_IDLE[0]);
+    this.loadImages(this.IMAGES_IDLE);
+    this.loadImages(this.IMAGES_WALKING);
+    this.loadImages(this.IMAGES_RUNNING);
+    this.loadImages(this.IMAGES_RUN_ATTACKING);
+    this.loadImages(this.IMAGES_RUN_SHOOTING);
+    this.loadImages(this.IMAGES_ATTACKING);
+    this.loadImages(this.IMAGES_SHOOTING);
+    this.loadImages(this.IMAGES_JUMPING);
+    this.loadImages(this.IMAGES_SITTING);
+    this.loadImages(this.IMAGES_BOMBING);
+    this.loadImages(this.IMAGES_TURNING_TO_RUN);
+    this.loadImages(this.IMAGES_TURNING_TO_WALK);
+    this.loadImages(this.IMAGES_DYING);
+    this.animate(this.IMAGES_IDLE, 100);
     this.x = x;
     this.y = y;
     this.mirrorX = true;
