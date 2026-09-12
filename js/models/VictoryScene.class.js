cd2a58a3a0270c8af323a52596b4fa19376fecbc
Quirin Pflaum <quirin.pflaum@proton.me>
2026-09-12 13:46:10 +0200

    implemented final ScrollingText, explaining the victory and congratulating

--- js/models/VictoryScene.class.js
+++ js/models/VictoryScene.class.js
@@ -9,6 +9,9 @@ class VictoryScene {
     this.character.y = this.character.groundY;
     this.character.speedY = 0;
 
+    this.lastUpdatedAt = performance.now();
+    this.endingText = null;
+
     this.treeX =
       this.character.x + this.world.cameraX + this.character.width / 2 - 175;
 
@@ -34,10 +37,18 @@ class VictoryScene {
   }
 
   update() {
+    const now = performance.now();
+    const deltaTime = now - this.lastUpdatedAt;
+    this.lastUpdatedAt = now;
+
     if (this.state === "waitAfterBow") {
       this.waitAfterBow();
     } else if (this.state === "sink") {
       this.sinkCharacter();
+    }
+
+    if (this.endingText) {
+      this.endingText.update(deltaTime);
     }
   }
 
@@ -57,15 +68,29 @@ class VictoryScene {
   }
 
   draw() {
-    if (!this.tree) return;
+    if (this.tree) {
+      this.world.drawObject(this.tree);
+    }
 
-    this.world.drawObject(this.tree);
+    if (this.endingText) {
+      this.endingText.draw();
+    }
   }
 
   async startTreeGrowing() {
     this.tree = new GrowingTree(this.treeX, this.treeY);
 
     await this.tree.waitForImages();
+
+    this.endingText = new ScrollingText(
+      this.world.ctx,
+      "CONGRATULATIONS! PEACE HAS RETURNED TO THE FOREST. ENTIE HAS FOUND PEACE AT LAST AND RETURNS TO HIS COMMUNITY IN THE WOODS.",
+      {
+        height: 32,
+        speed: 52,
+        y: this.world.canvas.height - 26,
+      },
+    );
 
     this.state = "treeGrowing";
     this.tree.grow();
